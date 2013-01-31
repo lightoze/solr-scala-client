83fe5b06680ec5623a554a48a3b1855ee862a7f3
Vladimir Kulev <vladimir.kulev@teklabs.com>
2013-01-31 12:17:38 +0200

    Update to SolrJ 4.1.0

--- src/main/scala/jp/sf/amateras/solr/scala/SolrServerFactory.scala
+++ src/main/scala/jp/sf/amateras/solr/scala/SolrServerFactory.scala
@@ -3,12 +3,15 @@ package jp.sf.amateras.solr.scala
 import org.apache.solr.common._
 import org.apache.solr.common.util._
 import org.apache.solr.client.solrj._
-import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer
-import org.apache.commons.httpclient.auth.AuthScope
-import org.apache.commons.httpclient.UsernamePasswordCredentials
+import impl.HttpSolrServer
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.AbstractHttpClient
+import org.apache.http.{HttpRequest, HttpRequestInterceptor}
+import org.apache.http.protocol.HttpContext
+import org.apache.http.impl.auth.BasicScheme
 
 object SolrServerFactory {
-  
+
   /**
    * Configure SolrClient for basic authentication.
    *
@@ -18,23 +21,21 @@ object SolrServerFactory {
    * }}}
    */
   def basicAuth(username: String, password: String) = (url: String) => {
-      val server = new CommonsHttpSolrServer(url)
-      
-      val cred = new UsernamePasswordCredentials(username, password)
-      val jurl = new java.net.URL(server.getBaseURL())
-
-      val client = server.getHttpClient()
-      client.getState().setCredentials(new AuthScope(jurl.getHost(), jurl.getPort(), AuthScope.ANY_REALM), cred)
-      client.getParams().setAuthenticationPreemptive(true)
-      
-      server
+    val server = new HttpSolrServer(url)
+    val credentials = new UsernamePasswordCredentials(username, password)
+    server.getHttpClient.asInstanceOf[AbstractHttpClient].addRequestInterceptor(new HttpRequestInterceptor {
+      def process(request: HttpRequest, context: HttpContext) {
+        request.addHeader(BasicScheme.authenticate(credentials, "US-ASCII", false))
+      }
+    })
+    server
   }
-  
+
   /**
    * Provides the dummy SolrServer for unit testing.
-   * 
+   *
    * {{{
-   * implicit val server = SolrServerFactory.dummt { request =>
+   * implicit val server = SolrServerFactory.dummy { request =>
    *   println(request.getMethod)
    *   println(request.getPath)
    *   println(request.getParams)
@@ -43,14 +44,16 @@ object SolrServerFactory {
    * }}}
    */
   def dummy(listener: (SolrRequest) => Unit) = (url: String) => new SolrServer {
-	def request(request: SolrRequest): NamedList[Object] = {
-	  listener(request)
-	  
-	  val response =  new SimpleOrderedMap[Object]()
-	  response.add("response", new SolrDocumentList())
-	  
-	  response
-	}
+    def request(request: SolrRequest): NamedList[Object] = {
+      listener(request)
+
+      val response = new SimpleOrderedMap[Object]()
+      response.add("response", new SolrDocumentList())
+
+      response
+    }
+
+    def shutdown() {}
   }
-  
+
 }
